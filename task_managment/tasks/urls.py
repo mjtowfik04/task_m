df2c891b5b72c4dcf89173267a1e624d663b0001
towfik <mjtowfik659672@gmail.com>
2025-01-27 02:26:01 +0600

    add new fille

--- task_managment/tasks/urls.py
+++ task_managment/tasks/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from tasks.views import admin_dashboard
 from tasks.views import user_dashboard
+from tasks.views import test
 
 urlpatterns = [
     path('admin-dashboards',admin_dashboard),
-    path('user-dashboards',user_dashboard)
+    path('user-dashboards',user_dashboard),
+    path('test',test)
 ]
--- task_managment/tasks/views.py
+++ task_managment/tasks/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def admin_dashboard(request):
-    return render(request,"admin-dashboard.html")
+    return render(request,"deshboard/manager-deshboard.html")
 
 def user_dashboard(request):
-    return render(request,"user-dashboard.html")
+    return render(request,"deshboard/user-dashboard.html")
 
+def test(request):
+    return render(request,'test.html')
